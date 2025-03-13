f67008f39666d3b191ad31e59c2c3a6a606d0c32
Erfan-aminian <aminian158@gmail.com>
2025-03-13 13:41:15 -0400

    add register

--- flash/accounts/urls.py
+++ flash/accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
+from .views import UserCreateView
+
 app_name = "accounts"
 urlpatterns = [
     # Your URLs...
     path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', UserCreateView.as_view(), name='register'),
 ]
--- flash/accounts/views.py
+++ flash/accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions
+from rest_framework import status
+from rest_framework.generics import CreateAPIView
+from django.contrib.auth.models import User
+from .serializers import CreateUserProfileSerializer
+#Create your views here.
+class UserCreateView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = CreateUserProfileSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.validated_data
+        user_data = User(
+            username=data['username'],
+            email=data['email'],
+        )
+        user_data.set_password(data['password'])
+        user_data.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-# Create your views here.

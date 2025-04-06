67b2bd57c17ca84e18708752c2d73afaf4ef6f52
Erfan-aminian <aminian158@gmail.com>
2025-04-06 14:14:00 -0400

    Add model

--- flash/card/models.py
+++ flash/card/models.py
@@ -1,12 +1,23 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Deck(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='decks')
+
+    def __str__(self):
+        return self.name
+
 
 class FlashCardModel(models.Model):
     question = models.TextField()
     answer = models.TextField()
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE, related_name='cards')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return f"{self.question[:10]}... -> {self.answer[:10]}"
 

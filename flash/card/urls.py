a973165d08971867f9789a220ef00492fd55a366
Erfan-aminian <aminian158@gmail.com>
2025-04-07 13:28:25 -0400

    Add deck view and url

--- flash/card/urls.py
+++ flash/card/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import DeckView
 app_name = 'card'
 urlpatterns = [
-    pass
+    path('deck/', DeckView.as_view(), name='deck'),
 ]
--- flash/card/views.py
+++ flash/card/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .serializers import DeckSerializer, FlashCardSerializer
+from .models import DeckModel, FlashCardModel
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
 
 # Create your views here.
+class DeckView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request):
+        decks = DeckModel.objects.filter(user=request.user)
+        serializer = DeckSerializer(decks, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = DeckSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

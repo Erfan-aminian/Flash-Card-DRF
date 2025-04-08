33d86bfe34cbc2cbadd1ed0f3953776824ad5158
Erfan-aminian <aminian158@gmail.com>
2025-04-08 14:45:05 -0400

    Add deck detail view

--- flash/card/views.py
+++ flash/card/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from .serializers import DeckSerializer, FlashCardSerializer
 from .models import DeckModel, FlashCardModel
@@ -21,3 +22,32 @@ class DeckView(APIView):
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class DeckDetailView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self, pk, user):
+        try:
+            return DeckModel.objects.get(pk=pk, user=user)
+        except DeckModel.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        deck = self.get_object(pk, request.user)
+        serializer = DeckSerializer(deck)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        deck = self.get_object(pk, request.user)
+        serializer = DeckSerializer(deck, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        deck = self.get_object(pk, request.user)
+        deck.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
